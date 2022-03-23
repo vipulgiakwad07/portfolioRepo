7bafaf28fdc5aa402f9daa265554e4a7089adbe4
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-23 18:58:25 +0530

    Add files via upload

---
+++ src/server/skills/skills_controller.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+
+var Skills = require("./skills_schema");
+mongoose.connect(`mongodb://127.0.0.1:27017/portfolio`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+});
+
+router.get("/", (req, res) => {
+    Skills.find({}, function(err, results) {
+        if (err) return;
+        res.json(results);
+    });
+});
+
+module.exports = router;
